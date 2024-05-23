39f6d41ef1726a238ad4834b3280b945f4874e32
Jackson12000 <160485340+Jackson12000@users.noreply.github.com>
2024-05-23 12:04:27 +0200

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="menustyle.css">
+	<title>onlinetutor</title>
+  <style>
+    .about{
+      color:white;
+    }
+  </style>
+</head>
+<body>
+
+<header>
+	<div class="menu_bar">
+  <img class="logo" src="online.jpg" alt="Logo">
+   
+		<ul>
+			<li><a href="menu.php"> Home</a></li>
+			<li><a href="about"> About Us</a></li>
+			<li><a href="contact.php"> Contact Us</a></li>
+			<li><a href="payment.php"> Payments</a></li>
+			<li><a href="subjectview.php"> Subject</a></li>
+			<li><a href="#"> Forms<i class="fas fa-caret-down"></i></a>
+				<div class="dropdown_menu">
+					<ul>
+					<li><a href="subject.php">Subjects<i class="fas fa-caret-right"></i></a>
+						<li><a href="booking.php">Booking</a></li>
+						<li><a href="lesson.php">Lesson</a></li>
+						<li><a href="tutor.php">Tutor </a></li>
+						<li><a href="notification.php">Notification<i class="fas fa-caret-right"></i></a></li>
+						<li><a href="student.php">Student </a></li>
+						<li><a href="review.php">Review </a></li>
+            <li><a href="session.php">Session </a></li>
+						<li><a href="lesson.php">Lesson </a></li>
+					</ul>
+				</div>
+        <li><a href="#"> Tables<i class="fas fa-caret-down"></i></a>
+				<div class="dropdown_menu">
+					<ul>
+          <li><a href="tutorview.php">Tutor View</a></li>
+						<li><a href="bookingview.php">Bookings<i class="fas fa-caret-right"></i></a>
+						<li><a href="sessionview.php">View Session</a></li>
+						<li><a href="notificationview.php">Notification view</a></li>
+						<li><a href="subjectview.php">Subject View</a></li>
+						<li><a href="reviewdata.php">riview information<i class="fas fa-caret-right"></i></a></li>
+						<li><a href="studentview.php">Student View</a></li>
+						<li><a href="paymentview.php">Payment View</a></li>
+					</ul>
+				</div>
+
+			</li>
+			<li><a href="#"> Setting</a>
+				<div class="dropdown_menu">
+					<ul>
+					<li><a href="user.php">Register</a></li>
+            <li><a href="userview.php">Edit Profile</a></li>
+						<li><a href="logout.php">Logout</a></li>
+					</ul>
+					
+				</div>
+
+			</li>
+		</ul>
+	</div>
+	</header>
+	<center>
+	<div>
+    <form action="" >
+    <h1 class="about">WELCOME ON ONLINE TUTORS MARKETPLACE</h1></div> <br>
+  <div><h2>ABUOUT USA</h2>
+<h3 class="about">An <strong> ONLINE TUTOR</strong> marketplace is a web-based platform that connects<br><br> students with 
+qualified tutors for personalized learning experiences.<br><br>
+ These platforms feature detailed tutor profiles, advanced search </h3>
+ <h3 class="about"> and filter options, integrated booking systems,virtual classrooms,<br>  <strong>secure payment</strong> </h3>
+ <h3 class="about">processing, and review systems. They offer accessibility to tutors worldwide,</h3>
+ <h3 class="about"> flexible schedulingpersonalized instruction, competitive pricing, and the  <br><br>convenience
+ of online sessions, eliminating the need for travel.The process</h3>
+  <h3 class="about"> typically involves students signing up, searching for suitable tutors based on<br><br>
+  their needs,booking and paying for sessions through the platform, and </h3> 
+   <h3 class="about">classrooms. Students can leave feedback after sessions, <br><br>
+    helping maintain high teaching standards. Examples of such platforms <br><br>
+    include Tutor.com, Wyzant, Chegg Tutors, and Preply, </h3>
+    <h3 class="about">all of which enhance educational opportunities by leveraging <br><br>
+    technology for effective and accessible tutoring services</h3>
+</form >
+
+</div>
+<div style="color: black;font-size: 25px;"><p>Connect with us on social media or social networkings:
+
+  <a href="https://www.twitter.com/pdas" style="color: white;font-size: 15px; text-decoration:none; background-color:blue;">TWITTER</a>
+  <a href="https://www.facebook.com/pdas" style="color: white;font-size: 15px;text-decoration:none;background-color: blue;" >FACEBOOK</a>
+  
+  <a href="https://www.linkedin.com/pdas" style="color: white;font-size: 15px;background-color: blue;text-decoration:none">LINKEDIN</a>
+  <a href="https://www.whatapp.com/pdas" style="color: white;font-size: 15px;background-color: blue; text-decoration:none">WHATAPP</a>
+</p></div>
+
+<div><footer style="background-color:rgb(25, 36, 193);text-align: center;width:100%;height:50px; color: white;font-size: 15px;margin-top: 20px;"><p> Designed by Murazeyesu Jackson_222010127 &copy YEAR TWO BIT GROUP A &reg 2024</p></footer></div>
+</center>
+	<body>
+    <html>
